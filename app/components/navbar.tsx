afa3e64bee35881691abccb7f28207cf3522f155
Matthew Pandolfo <matt@pandolfo.com>
2024-08-04 20:53:00 -0400

    navbar

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -1,18 +1,64 @@
-import React from "react";
+"use client";
+
+import React, { useState } from "react";
 import { bitter } from "../utils/fonts";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBars } from "@fortawesome/free-solid-svg-icons";
+import { AnimatePresence, motion } from "framer-motion";
+import Link from "next/link";
 
 function Navbar() {
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+
   return (
-    <nav className="h-nav flex justify-between items-center px-5 backdrop-blur bg-white/50 w-[90%] rounded-md mt-[5px] fixed left-1/2 right-1/2 -translate-x-1/2">
-      <p className={`${bitter.className} text-primary text-md font-bold`}>
-        HEENA KAPADIA
-      </p>
-      <FontAwesomeIcon
-        icon={faBars}
-        className="w-6 cursor-pointer text-secondary"
-      />
+    <nav className="min-h-[75px] flex flex-col justify-center items-center px-5 backdrop-blur bg-white/50 w-[90%] rounded-md mt-[5px] fixed left-1/2 right-1/2 -translate-x-1/2">
+      <div className="w-full flex items-center justify-between h-nav">
+        <p className={`${bitter.className} text-primary text-md font-bold`}>
+          HEENA KAPADIA
+        </p>
+        <FontAwesomeIcon
+          onClick={() => setIsOpen(!isOpen)}
+          icon={faBars}
+          className="cursor-pointer text-secondary h-6"
+        />
+      </div>
+      <AnimatePresence>
+        {isOpen && (
+          <motion.div
+            initial={{ height: 0 }}
+            animate={{ height: "auto" }}
+            exit={{ height: 0 }}
+            className="flex flex-col gap-2 text-center text-md font-bold text-secondary pb-8">
+            <Link href="/">
+              <motion.p
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                exit={{ opacity: 0, transition: { delay: 0.2 } }}
+                transition={{ delay: 0.1 }}>
+                Home
+              </motion.p>
+            </Link>
+            <Link href="/contact">
+              <motion.p
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                exit={{ opacity: 0, transition: { delay: 0.1 } }}
+                transition={{ delay: 0.2 }}>
+                Contact
+              </motion.p>
+            </Link>
+            <Link href="/about">
+              <motion.p
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                exit={{ opacity: 0, transition: { delay: 0.0 } }}
+                transition={{ delay: 0.3 }}>
+                About
+              </motion.p>
+            </Link>
+          </motion.div>
+        )}
+      </AnimatePresence>
     </nav>
   );
 }
