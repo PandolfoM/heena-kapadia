56c53421f5ab7fc63b363fd86e729117d2494e60
PandolfoM <matt@pandolfo.com>
2025-09-17 19:51:18 -0400

    viewport and metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,4 @@
-import type { Metadata } from "next";
+import type { Metadata, Viewport } from "next";
 import "./globals.css";
 import { nunitosans } from "./utils/fonts";
 import Navbar from "./components/navbar";
@@ -7,6 +7,53 @@ import Footer from "./components/footer";
 export const metadata: Metadata = {
   title: "Heena Kapadia",
   description: "Law office of Heena Kapadia",
+  openGraph: {
+    title: "Heena Kapadia",
+    description: "Law office of Heena Kapadia",
+    type: "website",
+    url: "https://heenakapadialaw.com/",
+    siteName: "Heena Kapadia",
+    locale: "en_US",
+    images: [
+      {
+        url: "https://heenakapadialaw.co/og.png",
+        width: 1200,
+        height: 630,
+        alt: "Heena Kapadia",
+      },
+    ],
+  },
+  twitter: {
+    title: "Heena Kapadia",
+    description: "Law office of Heena Kapadia",
+    images: [
+      {
+        url: "https://heenakapadialaw.com/og.png",
+        width: 1200,
+        height: 630,
+        alt: "Heena Kapadia",
+      },
+    ],
+  },
+  icons: {
+    icon: "/favicon.ico",
+    apple: "/apple-icon.png",
+  },
+  applicationName: "Heena Kapadia",
+  appleWebApp: {
+    title: "Heena Kapadia",
+    statusBarStyle: "default",
+    capable: true,
+  },
+};
+
+export const viewport: Viewport = {
+  themeColor: "#1f272d",
+  colorScheme: "light",
+  maximumScale: 1,
+  initialScale: 1,
+  width: "device-width",
+  viewportFit: "cover",
 };
 
 export default function RootLayout({
